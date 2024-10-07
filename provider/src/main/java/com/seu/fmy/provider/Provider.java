a720fbdddba9c08f38874472b19084c8d0a17d97
dream_Flying <84890202+fmy-fly@users.noreply.github.com>
2024-10-07 15:03:27 +0800

    完成本地注册服务器和序列化、反序列化

--- provider/src/main/java/com/seu/fmy/provider/Provider.java
+++ provider/src/main/java/com/seu/fmy/provider/Provider.java
@@ -1,11 +1,15 @@
 package com.seu.fmy.provider;
 
+import com.seu.fmy.common.service.UserService;
+import com.seu.fmy.rpc.registry.LocalRegistry;
 import com.seu.fmy.rpc.server.HttpServer;
 import com.seu.fmy.rpc.server.VertxHttpServer;
 
 
 public class Provider {
     public static void main(String [] args) {
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
